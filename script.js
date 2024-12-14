8d45367a3a50e4d6384eaf1639da0d46312b557e
DRS05 <daan307rosies@gmail.com>
2024-12-14 14:00:08 +0100

    feat(script): Added button and keyboard calc
    Added the functionality to calculate simplististic calculation through
    the usage of the calculator interface as well as keyboard functionality.
    Overall, I think the code looks really, however needs a little
    refactoring for optimization.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let operator = null;
-let operandOne = null;
-let operandTwo = null;
+let operator = "";
+let operandOne = "0";
+let operandTwo = "";
 
 
 function add(a, b) {
@@ -34,6 +34,140 @@ function operate(operandOne, operator, operandTwo) {
         case "/":
             return divide(operandOne, operandTwo);
         default:
-            return add(operandOne, operandTwo)
+            return add(operandOne, operandTwo);
     }
 }
+
+
+function handleOperandInput(number) {
+    if (operandOne === "0" && operator === "") {
+        operandOne = number;
+    } else if (operator === "") {
+        operandOne += number;
+    } else {
+        operandTwo += number;
+    }
+}
+
+
+function handleOperatorInput(symbol) {
+    if (operator === "" || operandTwo === "") {
+        operator = symbol;
+    } else {
+        calculateInput();
+        operandTwo = "";
+        operator = symbol;
+    }
+}
+
+
+function calculateInput() {
+    if (!(operator === "" || operandTwo === "")) {
+        operandOne = `${operate(Number.parseFloat(operandOne), operator, Number.parseFloat(operandTwo))}`;
+        operandTwo = "";
+        operator = "";
+    }
+}
+
+
+function clearInput() {
+    operandOne = "0";
+    operandTwo = "";
+    operator = "";
+}
+
+
+function dotCount(string) {
+    // count the number of occurences of dot in string
+    count = string.match(new RegExp(/\./, "g"));
+
+    if (count === null) {
+        return 0
+    }
+    return count.length
+}
+
+
+function decimalInput() {
+    if (operator === "" && dotCount(operandOne) === 0) {
+        operandOne += ".";
+    } else if (operandTwo.length >= 1 && dotCount(operandTwo) === 0) {
+        operandTwo += "."
+    }
+}
+
+
+function backSpaceInput() {
+    if (operandTwo.length >= 1) {
+        operandTwo = operandTwo.slice(0, operandTwo.length - 1);
+    } else if (operator.length === 1) {
+        operator = ""
+    } else if (operandOne != "0") {
+        operandOne = operandOne.slice(0, operandOne.length - 1);
+        if (operandOne.length === 0) {
+            operandOne = "0";
+        }
+    }
+}
+
+
+function isOperator(symbol) {
+    const operators = ["+", "-", "*", "/"];
+    return operators.includes(symbol);
+}
+
+
+const buttonContainer = document.querySelector(".button-container");
+const displayParagraph = document.querySelector(".display-paragraph");
+
+buttonContainer.addEventListener("click", event => {
+    const buttonClassName = event.target.className;
+
+    switch (buttonClassName) {
+        case "operand-button":
+            handleOperandInput(event.target.textContent);
+            break;
+        case "operator-button":
+            handleOperatorInput(event.target.textContent);
+            break;
+        case "calculate-button":
+            calculateInput();
+            break;
+        case "clear-button":
+            clearInput();
+            break;
+        case "decimal-button":
+            decimalInput();
+            break;
+        case "backspace-button":
+            backSpaceInput();
+            break;
+    }
+
+    displayParagraph.textContent = operandOne + " " + operator + " " + operandTwo;
+});
+
+document.addEventListener("keydown", event => {
+    const keyPressed = event.key;
+
+    console.log(keyPressed)
+
+    if (Number.parseInt(keyPressed) >= 0 && Number.parseInt(keyPressed) <= 9) {
+        handleOperandInput(keyPressed);
+    } else if (isOperator(keyPressed)) {
+        handleOperatorInput(keyPressed);
+    } else if (keyPressed === "Enter") {
+        calculateInput();
+    } else if (keyPressed === "Delete") {
+        console.log("Pressed Delete.")
+        clearInput();
+    } else if (keyPressed === ".") {
+        decimalInput();
+    } else if (keyPressed === "Backspace") {
+        backSpaceInput();
+    }
+
+    displayParagraph.textContent = operandOne + " " + operator + " " + operandTwo;
+})
+
+
